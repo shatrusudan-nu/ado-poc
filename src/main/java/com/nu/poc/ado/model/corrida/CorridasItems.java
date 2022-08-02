dd2c23fee79c58a0a4a505b8622939a6533f6191
alberto-perez-meneses <dr.kidb@gmail.com>
2022-08-01 21:01:18 -0500

    it's added the functionality to calculate the number of seats in corridas collection

---
+++ src/main/java/com/nu/poc/ado/model/corrida/CorridasItems.java
@@ -0,0 +1,27 @@
+package com.nu.poc.ado.model.corrida;
+
+import java.util.HashMap;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collection = "corridas")
+public class CorridasItems {
+
+    @Id
+    private String id;
+    public Contenido contenido;
+
+}
---
+++ src/main/java/com/nu/poc/ado/model/corrida/Salida.java
@@ -0,0 +1,24 @@
+package com.nu.poc.ado.model.corrida;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.annotation.Generated;
+import com.fasterxml.jackson.annotation.JsonAnyGetter;
+import com.fasterxml.jackson.annotation.JsonAnySetter;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Salida {
+
+    public List<Corrida> corridas = null;
+
+}
